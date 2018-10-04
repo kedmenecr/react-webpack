e5163d987c4ee677cb1520129989f6276788b348
Roberto Kedmenec <rexerd96@gmail.com>
2018-10-04 08:58:18 +0200

    New repo.

--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import * as ReactDom from 'react-dom'
-
-import { Hello } from './components/Hello'
-
-ReactDom.render(
-	<Hello compiler="TypeScript" framework="React" />,
-	document.getElementById("example")
-);
